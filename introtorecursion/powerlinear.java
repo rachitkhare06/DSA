528ba59e49ff066ef5c795dd0cb14f9c7f493910
RACHIT <rachit.vansh06@gmail.com>
2022-08-26 14:01:41 +0530

    update

---
+++ introtorecursion/powerlinear.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+public class powerlinear{
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int n=sc.nextInt();
+        System.out.println(power(x,n));
+    }
+
+    public static int power(int x, int n){
+        if(n==0)
+        {
+            return 1;
+        }
+        return x*power(x,--n);
+    }
+
+}
